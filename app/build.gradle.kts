7c856fa0e2e72628f9847c44726a53fb6f63e7a8
Galaxy2268 <danielsmulukinss@gmail.com>
2024-02-25 19:57:38 +0200

    added all necessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,14 @@ dependencies {
     implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
 }
